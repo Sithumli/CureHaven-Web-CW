29f3d4f4fd7dc4fa76d67d3b36f328e6a632418c
Sithumli Nanayakkara <sithumlimethaya2007@gmail.com>
2025-02-20 01:59:10 +0530

    background

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
+    const hamburger = document.querySelector('.hamburger');
     const nav = document.getElementById("nav-links");
     nav.classList.toggle("active");
+    hamburger.classList.toggle('active');
 }
