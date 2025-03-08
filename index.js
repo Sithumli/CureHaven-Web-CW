61218e135c6341d35a7e476da2d3df81fa35895c
Sithumli Nanayakkara <sithumlimethaya2007@gmail.com>
2025-03-08 11:08:30 +0530

    table

--- index.js
+++ index.js
@@ -5,18 +5,41 @@ function toggleMenu() {
     hamburger.classList.toggle('active');
 }
 
-const progressBar = document.querySelector('.progress-bar');
-        const loadingPercentage = document.querySelector('.loading-percentage');
 
-        let percentage = 0;
-        const interval = setInterval(() => {
-            percentage += 1; 
-            loadingPercentage.textContent = `${percentage}%`; 
-            progressBar.style.width = `${percentage}%`; 
 
-            console.log(loadingPercentage.textContent); 
+function openOverlay1() {
+    document.getElementById("overlay1").style.display = "block";
+}
 
-            if (percentage >= 100) {
-                clearInterval(interval); 
-            }
-        }, 30); 
+function openOverlay2() {
+    document.getElementById("overlay2").style.display = "block";
+}
+
+function openOverlay3() {
+    document.getElementById("overlay3").style.display = "block";
+}
+
+function openOverlay4() {
+    document.getElementById("overlay4").style.display = "block";
+}
+
+function openOverlay5() {
+    document.getElementById("overlay5").style.display = "block";
+}
+
+function openOverlay6() {
+    document.getElementById("overlay6").style.display = "block";
+}
+
+// Function to close specific overlays
+function closeOverlay(event) {
+    if (event) event.stopPropagation(); // Prevent event bubbling
+
+    // Close all overlays
+    document.getElementById("overlay1").style.display = "none";
+    document.getElementById("overlay2").style.display = "none";
+    document.getElementById("overlay3").style.display = "none";
+    document.getElementById("overlay4").style.display = "none";
+    document.getElementById("overlay5").style.display = "none";
+    document.getElementById("overlay6").style.display = "none";
+}
